05374e9398ffddad0bda23c6c43a7090343211e0
Nicolas <pnschlei@gmail.com>
2021-04-19 15:52:19 +0200

    create subtituteController class

---
+++ Controllers/substituteController.py
@@ -0,0 +1,9 @@
+# coding : utf-8
+import sys
+sys.path.append('C:\\Users\\Utilisateur\\Documents\\ExerciceOC\\Pur_Beurre')
+import Models.substitute as s
+
+class SubstituteController():
+
+	def __init__(self):
+		self.substitute = s.Substitute()
